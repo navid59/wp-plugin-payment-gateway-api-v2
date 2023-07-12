94d510a2444aeb873d5844b22914a597a8febe29
Navid <navid@netopia-system.com>
2023-07-12 16:08:11 +0300

    Update

--- wc-netopiapayments-auth.php
+++ wc-netopiapayments-auth.php
@@ -51,8 +51,7 @@ function getCredentialCallback($request)
      */
 
      $ntpAccessRes = ntpPlatformnLogin($params);
-    //  die(var_dump($ntpAccessRes));
-     if($ntpAccessRes['code'] !== 200) {
+     if ($ntpAccessRes['code'] !== 200) {
         $data = array(
             'status' => false,
             'message' => $ntpAccessRes['message'],
@@ -93,108 +92,13 @@ function validateParams(array $params)
 
 function ntpPlatformnLogin($params) {
     // JSON data to send
-        $data = [
-                'login' => [
-                    'username' => $params['username'],
-                    'password' => $params['password'],
-                    'code'     => ''
-                ]
-            ];
-
-        // Convert data to JSON format
-        $jsonData = json_encode($data);
-
-        // Initialize cURL
-        $ch = curl_init();
-
-        // Set the URL
-        $url = 'https://admin.netopia-payments.com/api/auth/login';
-        curl_setopt($ch, CURLOPT_URL, $url);
-
-        // Set the request method to POST
-        curl_setopt($ch, CURLOPT_POST, 1);
-
-        // Set the request body (JSON data)
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-
-        // Include headers in the response
-        curl_setopt($ch, CURLOPT_HEADER, 1);
-
-        // Return the response instead of outputting it
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        // Execute the request
-        $response = curl_exec($ch);
-
-        // Check for errors
-        if ($response === false) {
-            die('cURL Error: ' . curl_error($ch));
-        }
-
-        // Separate headers and body
-        list($headers, $body) = explode("\r\n\r\n", $response, 2);
-
-        // Extract cookies from the headers
-        preg_match_all('/^set-cookie:\s*([^;]*)/mi', $headers, $matches);
-        $cookies = [];
-        foreach ($matches[1] as $match) {
-            parse_str($match, $cookie);
-            $cookies = array_merge($cookies, $cookie);
-        }
-
-        // Close cURL
-        curl_close($ch);
-
-        // Define NTP response
-        $ntpPlatformResponse = array();
-
-        // get response body
-        $bodyArr = json_decode($body, true);
-
-        // Check response HTTP Code
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        if($httpCode != 200) {
-            $ntpPlatformResponse['code'] = $httpCode;
-            $ntpPlatformResponse['message'] = $bodyArr['message'];
-            $ntpPlatformResponse['data'] = array();
-            return $ntpPlatformResponse;
-        }
-
-        // Handle the extracted cookies
-        if(!array_key_exists('ADM_SESS_ID', $cookies)) {
-            $ntpPlatformResponse['code'] = 404;
-            $ntpPlatformResponse['message'] = "Access key didn't found";
-            $ntpPlatformResponse['data'] = array();
-            return $ntpPlatformResponse;
-        }
-
-        if(is_null($cookies['ADM_SESS_ID']) || empty($cookies['ADM_SESS_ID'])) {
-            $ntpPlatformResponse['code'] = 406;
-            $ntpPlatformResponse['message'] = "Access key is Not Acceptable";
-            $ntpPlatformResponse['data'] = array();
-            return $ntpPlatformResponse;
-        }
-
-        // return access key
-        $ntpPlatformResponse['code'] = $httpCode;
-        $ntpPlatformResponse['message'] = "";
-        $ntpPlatformResponse['data']['accessKey'] = $cookies['ADM_SESS_ID'];
-        return $ntpPlatformResponse;
-}
-
-function getNtpSignature($accessKey) {
-    // URL to send the request
-    $url = 'https://admin.netopia-payments.com/api/pos/list';
-
-    // Custom headers
-    $headers = [
-        'ADM_SESS_ID' =>  $accessKey
-    ];
-
-    // JSON data to send
-    $data = [];
-
-    // die(var_dump($headers));
+    $data = [
+            'login' => [
+                'username' => $params['username'],
+                'password' => $params['password'],
+                'code'     => ''
+            ]
+        ];
 
     // Convert data to JSON format
     $jsonData = json_encode($data);
@@ -203,6 +107,7 @@ function getNtpSignature($accessKey) {
     $ch = curl_init();
 
     // Set the URL
+    $url = 'https://admin.netopia-payments.com/api/auth/login';
     curl_setopt($ch, CURLOPT_URL, $url);
 
     // Set the request method to POST
@@ -210,9 +115,6 @@ function getNtpSignature($accessKey) {
 
     // Set the request body (JSON data)
     curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-
-    // Set the custom headers
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
     // Include headers in the response
     curl_setopt($ch, CURLOPT_HEADER, 1);
@@ -225,28 +127,245 @@ function getNtpSignature($accessKey) {
 
     // Check for errors
     if ($response === false) {
-        die('cURL Error: ' . curl_error($ch));
+    die('cURL Error: ' . curl_error($ch));
+    }
+
+    // Separate headers and body
+    list($headers, $body) = explode("\r\n\r\n", $response, 2);
+
+    // Extract cookies from the headers
+    preg_match_all('/^set-cookie:\s*([^;]*)/mi', $headers, $matches);
+    $cookies = [];
+    foreach ($matches[1] as $match) {
+    parse_str($match, $cookie);
+    $cookies = array_merge($cookies, $cookie);
     }
 
     // Close cURL
     curl_close($ch);
 
-    // Display the response
-    echo $response;
+    // Define NTP response
+    $ntpPlatformResponse = array();
+
+    // get response body
+    $bodyArr = json_decode($body, true);
+
+    // Check response HTTP Code
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    
+    if ($httpCode != 200) {
+        $ntpPlatformResponse['code'] = $httpCode;
+        $ntpPlatformResponse['message'] = $bodyArr['message'];
+        $ntpPlatformResponse['data'] = array();
+        // return json_encode($ntpPlatformResponse);
+        return $ntpPlatformResponse;
+    }
+
+    // Handle the General Error
+    if ($httpCode == 401) {
+        $ntpPlatformResponse['code'] = 401;
+        $ntpPlatformResponse['message'] = "Verific, if you already not signing in Platform. ";
+        $ntpPlatformResponse['data'] = array();
+        // return json_encode($ntpPlatformResponse);
+        return $ntpPlatformResponse;
+    }
+
+    // Handle the extracted cookies
+    if (!array_key_exists('ADM_SESS_ID', $cookies)) {
+        $ntpPlatformResponse['code'] = 404;
+        $ntpPlatformResponse['message'] = "Access key didn't found";
+        $ntpPlatformResponse['data'] = array();
+        // return json_encode($ntpPlatformResponse);
+        return $ntpPlatformResponse;
+    }
+
+    if (is_null($cookies['ADM_SESS_ID']) || empty($cookies['ADM_SESS_ID'])) {
+        $ntpPlatformResponse['code'] = 406;
+        $ntpPlatformResponse['message'] = "Access key is Not Acceptable";
+        $ntpPlatformResponse['data'] = array();
+        // return json_encode($ntpPlatformResponse);
+        return $ntpPlatformResponse;
+    }
+
+    // return access key
+    $ntpPlatformResponse['code'] = $httpCode;
+    $ntpPlatformResponse['message'] = "";
+    $ntpPlatformResponse['data']['accessKey'] = $cookies['ADM_SESS_ID'];
+    // return json_encode($ntpPlatformResponse);
+    return $ntpPlatformResponse;
+}
+
+function getNtpSignature($accessKey) {
+    // JSON data to send
+    $jsonData = '{}';
+
+    // Initialize cURL
+    $ch = curl_init();
+
+    // Set the URL
+    $url = 'https://sandbox.netopia-payments.com/api/pos/list';
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    // Set the request method to POST
+    curl_setopt($ch, CURLOPT_POST, 1);
+
+    // Set the request body (JSON data)
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+
+    // Include headers in the response
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+
+    // Return the response instead of outputting it
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 
+    // Set the cookies
+    $cookies = 'ADM_SESS_ID='.$accessKey;
+    curl_setopt($ch, CURLOPT_COOKIE, $cookies);
 
-    // return array([
-    //     "pos" => "AAAA-BBBB-CCCC-DDDD-EEEE",
-    //     "active" => true,
-    // ],[
-    //     "pos" => "EEEE-DDDD-CCCC-BBBB-AAAA",
-    //     "active" => false,
-    // ]);
+    // Execute the request
+    $response = curl_exec($ch);
+
+    // Check for errors
+    if ($response === false) {
+    die('cURL Error: ' . curl_error($ch));
+    }
+
+    // Separate headers and body
+    list($headers, $body) = explode("\r\n\r\n", $response, 2);
+
+    // Extract cookies from the headers
+    preg_match_all('/^set-cookie:\s*([^;]*)/mi', $headers, $matches);
+    $cookies = [];
+    foreach ($matches[1] as $match) {
+    parse_str($match, $cookie);
+    $cookies = array_merge($cookies, $cookie);
+    }
+
+    // Close cURL
+    curl_close($ch);
+
+    // Define NTP response
+    $ntpPlatformResponse = array();
+
+    // get response body
+    $bodyArr = json_decode($body, true);
+
+    // Check response HTTP Code
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    if ($httpCode != 200) {
+        $ntpPlatformResponse['code'] = $httpCode;
+        $ntpPlatformResponse['message'] = $bodyArr['message'];
+        $ntpPlatformResponse['data'] = array();
+        // return json_encode($ntpPlatformResponse);
+        return $ntpPlatformResponse;
+    }
+
+    // Handle the extracted cookies
+    if (!array_key_exists('ADM_SESS_ID', $cookies)) {
+        $ntpPlatformResponse['code'] = 404;
+        $ntpPlatformResponse['message'] = "Access key didn't found";
+        $ntpPlatformResponse['data'] = array();
+        // return json_encode($ntpPlatformResponse);
+        return $ntpPlatformResponse;
+    }
+
+    if(is_null($cookies['ADM_SESS_ID']) || empty($cookies['ADM_SESS_ID'])) {
+        $ntpPlatformResponse['code'] = 406;
+        $ntpPlatformResponse['message'] = "Access key is Not Acceptable";
+        $ntpPlatformResponse['data'] = array();
+        // return json_encode($ntpPlatformResponse);
+        return $ntpPlatformResponse;
+    }
+
+    // return access key
+    $ntpPlatformResponse['code'] = $httpCode;
+    $ntpPlatformResponse['message'] = "";
+    $ntpPlatformResponse['data'] = $bodyArr;
+    // return json_encode($ntpPlatformResponse);
+    return $ntpPlatformResponse;
 }
 
 function getNTPApiKey($accessKey, $isLive) {
-    return array($accessKey);
+    // Initialize cURL
+    $ch = curl_init();
+
+    // Set the URL
+    $url = 'https://admin.netopia-payments.com/api/user/api/key/get';
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    // Set the request method to GET
+    curl_setopt($ch, CURLOPT_HTTPGET, true);
+
+    // Include headers in the response
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+
+    // Return the response instead of outputting it
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+
+    // Set the cookies
+    $cookies = 'ADM_SESS_ID='.$accessKey;
+    curl_setopt($ch, CURLOPT_COOKIE, $cookies);
+
+    // Execute the request
+    $response = curl_exec($ch);
+
+    // Check for errors
+    if ($response === false) {
+    die('cURL Error: ' . curl_error($ch));
+    }
+
+    // Separate headers and body
+    list($headers, $body) = explode("\r\n\r\n", $response, 2);
+
+    // Extract cookies from the headers
+    preg_match_all('/^set-cookie:\s*([^;]*)/mi', $headers, $matches);
+    $cookies = [];
+    foreach ($matches[1] as $match) {
+    parse_str($match, $cookie);
+    $cookies = array_merge($cookies, $cookie);
+    }
+
+    // Close cURL
+    curl_close($ch);
+
+    // Define NTP response
+    $ntpPlatformResponse = array();
+
+    // get response body
+    $bodyArr = json_decode($body, true);
+
+    // Check response HTTP Code
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    if ($httpCode != 200) {
+        $ntpPlatformResponse['code'] = $httpCode;
+        $ntpPlatformResponse['message'] = $bodyArr['message'];
+        $ntpPlatformResponse['data'] = array();
+        return $ntpPlatformResponse;
+    }
+
+    // Handle the extracted cookies
+    if (!array_key_exists('ADM_SESS_ID', $cookies)) {
+        $ntpPlatformResponse['code'] = 404;
+        $ntpPlatformResponse['message'] = "Access key didn't found";
+        $ntpPlatformResponse['data'] = array();
+        return $ntpPlatformResponse;
+    }
+
+    if (is_null($cookies['ADM_SESS_ID']) || empty($cookies['ADM_SESS_ID'])) {
+        $ntpPlatformResponse['code'] = 406;
+        $ntpPlatformResponse['message'] = "Access key is Not Acceptable";
+        $ntpPlatformResponse['data'] = array();
+        return $ntpPlatformResponse;
+    }
+
+    // return access key
+    $ntpPlatformResponse['code'] = $httpCode;
+    $ntpPlatformResponse['message'] = "";
+    $ntpPlatformResponse['data'] = $bodyArr;
+    return $ntpPlatformResponse;
 }
 
 ?>
